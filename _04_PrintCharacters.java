93441e2671a226cb7bff3960a4a2e8e53e18d025
bouboubou <grifona00@gmail.com>
2016-03-22 12:40:01 +0200

    Upload files

---
+++ _04_PrintCharacters.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class _04_PrintCharacters {
+    public static void main(String[] args) {
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
